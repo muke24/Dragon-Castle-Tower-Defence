412a36693f948f1f3c501ecf28dd961592dd6050
Peter <pjthompson.au@gmail.com>
2019-10-24 15:47:21 +1100

    Torch's Added

---
+++ Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour
+{
+    public Transform enemyPrefab;
+
+    public float timeBetweenWaves = 5f;
+    private float countdown = 2f;
+
+
+    void Update()
+    {
+        if (countdown <= 0f)
+        {
+            spawnWave();
+            countdown = timeBetweenWaves;
+        }
+        countdown -= Time.deltaTime;
+    }
+    
+    void spawnWave()
+    {
+        Debug.Log("Wave Incoming");
+    }
+
+}
