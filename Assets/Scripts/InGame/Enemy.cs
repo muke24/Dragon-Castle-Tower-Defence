c5d238421e222210619640c131ecfb7c5a28b28c
Peter <pjthompson.au@gmail.com>
2019-10-25 14:25:43 +1100

    TurretsAdded

---
+++ Assets/Scripts/InGame/Enemy.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float Speed = 10f;
+
+    private Transform target;
+    private int wavepointIndex = 0;
+
+    void Start()
+    {
+        target = Waypoints.points[0];
+    }
+
+    void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * Speed * Time.deltaTime, Space.World);
+
+        if (Vector3.Distance(transform.position, target.position) <= 0.2f)
+        {
+            GetNextWaypoint();
+        }
+    }
+
+    void GetNextWaypoint()
+    {
+        if (wavepointIndex >= Waypoints.points.Length - 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        wavepointIndex++;
+        target = Waypoints.points[wavepointIndex];
+    }
+}
