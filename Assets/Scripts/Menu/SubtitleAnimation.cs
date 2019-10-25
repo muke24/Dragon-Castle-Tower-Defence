6bf1c056d05cbe1466a52149ba6cb4f2e6949e1e
Peter <pjthompson.au@gmail.com>
2019-10-25 18:09:17 +1100

    MenuEdits

---
+++ Assets/Scripts/Menu/SubtitleAnimation.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SubtitleAnimation : MonoBehaviour
+{
+    public GameObject mainMenuSubtitle;
+    public GameObject menu;
+    public GameObject settings;
+
+    public bool optionsEnabled;
+
+    public Animator subtitleAnimation;
+    public AnimatorControllerParameter subtitleStopped;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        optionsEnabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (menu.activeInHierarchy == true)
+        {
+            optionsEnabled = false;
+        }
+
+        if (settings.activeInHierarchy == true)
+        {
+            optionsEnabled = true;
+        }
+
+        if (optionsEnabled == true)
+        {
+            mainMenuSubtitle.
+        }
+
+    }
+
+}
