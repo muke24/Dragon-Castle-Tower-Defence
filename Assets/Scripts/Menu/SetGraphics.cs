885bc3a92c221d73e5fc0a2a7e7d51f73cf3c9fb
Peter <pjthompson.au@gmail.com>
2019-11-14 15:56:52 +1100

    stuff

---
+++ Assets/Scripts/Menu/SetGraphics.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetGraphics : MonoBehaviour
+{
+    public Resolution resolution;
+    public Text resolutionText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        resolutionText = resolution.ToString;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Screen.SetResolution
+    }
+}
