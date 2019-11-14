1910ad0f01ad58e130180361ea3b36b379d334b9
Peter <pjthompson.au@gmail.com>
2019-11-14 15:16:35 +1100

    MenuStuff

--- Assets/Scripts/Menu/MenuHandler.cs
+++ Assets/Scripts/Menu/MenuHandler.cs
@@ -1,18 +1,14 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
+using System.Collections;
+
+using UnityEngine.SceneManagement;
 
 public class MenuHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    public void LoadScene(int level)
     {
-        
+        SceneManager.LoadScene("Game");
     }
 }
