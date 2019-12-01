ee9f28d9f1811b22055a7c6a9e2d1791b36ab0f4
Eddie <ed.dixen@gmail.com>
2019-12-02 04:59:21 +1100

    placement fix

--- Assets/Scripts/InGame/Towers/TowerManager.cs
+++ Assets/Scripts/InGame/Towers/TowerManager.cs
@@ -4,19 +4,22 @@ using UnityEngine;
 
 public class TowerManager : MonoBehaviour
 {
-    float distance = 10f;
+    public float distance = 25f;
 
     Vector3 mousePosition, targetPosition;
 
     //Used to instantiate an object at mouse position
     public Transform tempObject;
+    private GameObject linehandler;
 
     void Update()
     {
         if (Input.GetMouseButtonUp(0))
         {
             // place a tower at mouse point
-            PlaceTower(UnityEngine.Random.Range(0, 3));
+            PlaceTower2(UnityEngine.Random.Range(0, 4));
+            //PlaceTower(UnityEngine.Random.Range(0, 3));
+            //PlaceTower1(UnityEngine.Random.Range(0, 3));
         }
     }
 
@@ -39,7 +42,39 @@ public class TowerManager : MonoBehaviour
 
         //Place the tower
         Instantiate(newTower.MeshName, tempObject.position, tempObject.transform.rotation);
-        
 
+
+    }
+
+
+    void PlaceTower1(int typeId)
+    {
+        //create a new tower of typeId
+        Tower newTower = TowerData.CreateTower(typeId);
+        Ray rayCast = Camera.main.ScreenPointToRay(Input.mousePosition);
+        linehandler = Instantiate(newTower.MeshName, rayCast.GetPoint(10), Quaternion.identity) as GameObject;
+        
+    }
+
+    void PlaceTower2(int typeId)
+    {
+        
+        Debug.Log("Left mouse clicked");
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Tower newTower = TowerData.CreateTower(typeId);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.transform.parent.name == "Maze_1")
+            {
+                hit.point.Set(hit.point.x, hit.point.y + 10f, hit.point.z);
+
+                Instantiate(newTower.MeshName, hit.point, Quaternion.identity);
+
+                print("My object is clicked by mouse");
+            }
+        }
+        
     }
 }
